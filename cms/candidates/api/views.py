b5aae999d0eb3e9db899865375aa596fe2bdcbf6
Affan <affan.muddassir10@gmail.com>
2020-06-07 21:13:57 +0500

    Added PEP8 style comments across various files

--- cms/candidates/api/views.py
+++ cms/candidates/api/views.py
@@ -1,13 +1,8 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework import generics
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.filters import SearchFilter, OrderingFilter
-from django.contrib.auth.models import User
 import os
 
 from ..models import CandidateModel
@@ -16,8 +11,27 @@ from .serializers import CandidateSerializer
 
 @permission_classes((IsAuthenticated,))
 class CandidateAPIView(APIView):
+    """
+        - This class handles all GET / POST / PUT / DELETE requests and returns appropriate
+          responses through the Django REST Framework.
+        - All methods defined below require an authenticated request i.e. User must be logged-in
+          to access these methods.
+        - Once an authenticated request is received, they appropriately handle them based on
+          validations present in the system and return appropriate HTTP Status Codes and
+          JSON Responses.
+    """
 
     def get(self, request, id=None):
+        """
+            This method handles GET requests to:
+            - Get all candidates.
+            - Get a specific candiate.
+
+            :param request: standard request object that contains details about the request received.
+            :param id: id of the candidate. Is initialized to None if no id is present in the request.
+            :return: list of candidates or a single candidate based on the GET request.
+                    - HTTP 404 Not Found Error Code in case of failure.
+        """
         if id is not None:
             try:
                 candidate = CandidateModel.objects.get(id=id)
@@ -31,6 +45,12 @@ class CandidateAPIView(APIView):
         return Response(serializer.data)
 
     def post(self, request):
+        """
+            This method handles POST requests to add new candidates.
+            :param request: standard request object that contains details about the request received.
+            :return: newly created Candidate object in case of success.
+                    - HTTP 400 Bad Request Code in case of failure.
+        """
         candidate  = CandidateModel()
         serializer = CandidateSerializer(candidate, data=request.data)
         if serializer.is_valid():
@@ -39,6 +59,15 @@ class CandidateAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, id):
+        """
+            This method handles PUT requests to update a specific candidate.
+
+            :param request: standard request object that contains details about the request received.
+            :param id: id of the candidate to be updated.
+            :return: updated candidate in case of success.
+                    - HTTP 404 Not Found Error Code in case candidate with the id requested is not found.
+                    - HTTP 400 Bad Request Error Code in case of failure.
+        """
         try:
             candidate = CandidateModel.objects.get(id=id)
         except CandidateModel.DoesNotExist:
@@ -51,6 +80,14 @@ class CandidateAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, id):
+        """
+            This method handles DELETE requests to delete a specific candidate.
+
+            :param request: standard request object that contains details about the request received.
+            :param id: id of the candidate to be deleted.
+            :return: HTTP 204 No Content Status Code on successful deletion.
+                    - HTTP 404 Not Found Error Code in case candidate with the id requested is not found.
+        """
         try:
             candidate = CandidateModel.objects.get(id=id)
         except CandidateModel.DoesNotExist:
--- cms/candidates/models.py
+++ cms/candidates/models.py
@@ -2,16 +2,28 @@ from django.db import models
 from django.core.validators import FileExtensionValidator
 import os
 
-# Create your models here.
-
 
 def rename_and_save(instance, filename):
+    """
+        This function renames the resume of a candidate by adding his / her email
+        to the beginning of the filename before Django saves it to the filesystem
+        in the media folder.
+
+        :param instance: the candidate object it'll be working with.
+        :param filename: name of the resume of the corresponding candidate.
+        :return: path to the media folder with the updated resume name.
+    """
     path   = "media/"
     format = f'{instance.email}_{filename}'
     return os.path.join(path, format)
 
 
 class CandidateModel(models.Model):
+    """
+        Candidate Model definition.
+
+        To add JOBS to the UI, add these in the JOBS tuple defined below.
+    """
     JOBS = (
         ('Backend Engineer', 'Backend Engineer'),
         ('Frontend Engineer', 'Frontend Engineer'),
